a4d53d89356c486dbe832a0825ba9e045139a026
iconicig1234 <ipshitagupta8181@gmail.com>
2025-01-09 22:27:43 +0530

    chat-app-changes-done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Footer />
+    </>
+  )
+}
+
+export default App
